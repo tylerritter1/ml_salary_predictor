f9b23a902ff56e3daf44a5be03252d2e755c9377
Tyler Ritter <tyler.ritter@instacart.com>
2019-04-09 16:14:20 -0700

    new files

---
+++ model.py
@@ -0,0 +1,8 @@
+import numpy as np, pandas as pd, pickle, requests, json
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+import pandas as pd, pickle, requests
+
+
+dataset = pd.read_csv('Salary_Data.csv')
